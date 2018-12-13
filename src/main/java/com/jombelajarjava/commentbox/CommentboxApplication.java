99436638c3ca53fdc4cfb09198c4547787176c94
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-12-13 12:41:15 +0800

    Change indent to spaces

--- src/main/java/com/jombelajarjava/commentbox/CommentboxApplication.java
+++ src/main/java/com/jombelajarjava/commentbox/CommentboxApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommentboxApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CommentboxApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CommentboxApplication.class, args);
+    }
 }
 
