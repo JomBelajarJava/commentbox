18c18e3095811f755f8d2c64d490e793e996333d
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-12-13 12:36:18 +0800

    Init commit

---
+++ src/main/java/com/jombelajarjava/commentbox/CommentboxApplication.java
@@ -0,0 +1,14 @@
+package com.jombelajarjava.commentbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentboxApplication.class, args);
+	}
+
+}
+
